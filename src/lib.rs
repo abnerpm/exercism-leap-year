ca86b0f0ac06d8ddcd08be527f1acc483727ee96
Abner Magalhaes <abner.magalhaes95@outlook.com>
2020-09-19 19:57:58 -0300

    clean

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,3 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if is_divisible_by_four(&year) && is_not_divisible_by_one_hundred(&year) ||
-        is_divisible_by_one_hundred_and_four_hundred(&year) {
-            true
-        } else {
-            false
-        }
-}
-
-fn is_divisible_by_four(year: &u64) -> bool {
-    if year % 4 == 0 {
-        true
-    } else {
-        false
-    }
-}
-
-fn is_not_divisible_by_one_hundred(year: &u64) -> bool {
-    if year % 100 != 0 {
-        true
-    } else {
-        false
-    }
-}
-
-fn is_divisible_by_one_hundred_and_four_hundred(year: &u64) -> bool {
-    if year % 100 == 0 && year % 400 == 0 {
-        true
-    } else {
-        false
-    }
+    
 }
